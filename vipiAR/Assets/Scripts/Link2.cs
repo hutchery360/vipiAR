72cc633e17cf9ed441f4a4a0cea1dcea1a1c6148
Hutchery Live Studio <56971008+hutchery360@users.noreply.github.com>
2019-11-24 00:47:38 +0300

    First Commit
    Added Android AR Application made by Unity.

---
+++ vipiAR/Assets/Scripts/Link2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link2 : MonoBehaviour
+{
+    public void hutchery()
+    {
+        Application.OpenURL("https://www.youtube.com/playlist?list=PL9Zoc0HbHhFfIX05K9mJc_ke2l9BXKG51");
+    }
+    public void youtube()
+    {
+        Application.OpenURL("https://youtu.be/Xts5eZ2hIXg");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
